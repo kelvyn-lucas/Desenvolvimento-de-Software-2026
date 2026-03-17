2561074179d8131ba7d12520e145e372654fd829
kelvyn-lucas <146493217+kelvyn-lucas@users.noreply.github.com>
2026-03-16 21:46:54 -0300

    Add files via upload

---
+++ Curso em video JAVA/Lista de exercicios/src/Area.java
@@ -0,0 +1,8 @@
+public class Area {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Calcular a area ");
+        int base = 10, altura = 20;
+        int area = (base * altura)   / 2;
+        System.out.println( area );
+    }
+}
---
+++ Curso em video JAVA/Lista de exercicios/src/IdiomaSistema.java
@@ -0,0 +1,9 @@
+import java.lang.reflect.AccessFlag.Location;
+
+public class IdiomaSistema {
+    public static void main(String[] args) {
+        Location idioma = new Location();
+        System.out.println("O idioma do sistema é: ");
+        System.out.println(idioma.toString());
+    }
+}
