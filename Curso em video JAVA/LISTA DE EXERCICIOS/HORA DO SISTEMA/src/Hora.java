c5bbb5120f3cfdba4c5c734e4e6bc9ed716ca37d
kelvyn-lucas <146493217+kelvyn-lucas@users.noreply.github.com>
2026-04-06 21:27:18 -0300

    Add files via upload

---
+++ Curso em video JAVA/LISTA DE EXERCICIOS/HORA DO SISTEMA/src/Hora.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Hora {
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A data do sitema é: ");
+        System.out.println(relogio.toString());
+    }
+}
---
+++ Curso em video JAVA/LISTA DE EXERCICIOS/MEDIA/src/Medianota.java
@@ -0,0 +1,8 @@
+public class Medianota {
+    public static void main(String[] args){
+        System.out.println("Média nota da sala ");
+        int n1 = 10, n2 = 10, n3 = 10, n4 = 10, media;
+         media = (n1+n2+n3+n4)/4;
+         System.out.println(media);
+    }
+}
